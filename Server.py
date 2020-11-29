d08f57f0ba57483e2706ae3642841126fddb19d2
saeidnahali <63702251+saeidnahali@users.noreply.github.com>
2020-11-28 17:28:38 -0800

    Ping IP
    Add new line for ping IP AWS server

--- Server.py
+++ Server.py
@@ -1,10 +1,16 @@
+import os
 class Server:
-    """ Server class for representing and manipulating servers. """
-
     def __init__(self, server_ip):
-        # TODO -
+        # TODO -""" Create a ServerClass with a server_ip instantiated"""
         self.server_ip = server_ip
-
-    def ping(self):
+    def get_server_ip(self):
         # TODO - Use os module to ping the server
-        return
+        return self.server_ip
+    def ping(self):
+         response = os.system("ping -n 4 -W 5 " + self.server_ip + " > /dev/null 2>&1")
+        # if command response is 0, then server is up and running
+         if response == 0:
+            return (self.server_ip + " is Up and Running")
+        # else the server is not running and is down
+         else:
+            return (self.server_ip + " is Down")
--- main.py
+++ main.py
@@ -4,10 +4,16 @@
 
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
-
+    print("Server Automator v0.1 by SAEID")
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
+    import os
+    name = "3.16.215.15"
+    response = os.system("ping -n 4 " + name)
     # TODO - Call Ping method and print the results
+    if response == 0:
+        print(name, 'is up!')
+    else:
+        print(name, 'is down!')
