32d186af12f3f0c5231f7a3918e018876b13a33a
saeidnahali <63702251+saeidnahali@users.noreply.github.com>
2020-12-07 22:33:37 -0800

    NEW edit

--- main.py
+++ main.py
@@ -2,23 +2,40 @@
 # Zachary Rubin, zrubin@rtc.edu
 # CNA 337 Fall 2020
 # I worked with Vlado, Abdi, Dorin and Igor, Mohammad on this code.
-# Liviu Patrisco (Liviu_patrisco@hotmail.com) helped us to write the code.
+# Liviu Patrasco (liviu_patrasco@hotmail.com) helped us to write the code.
+
 
 from Server import Server
 
 
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by SAEID")
+    print("Server Automator v0.1 by SAEID NAHALI")
+
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-    ec2_ip_address = "3.16.215.15"
-    server = Server(ec2_ip_address)
-    result = server.ping()
-    print(result)
-    if result == 0:
-        print("Server with ip [%s] is up." % ec2_ip_address)
+    # TODO - Create a Server object
+    # TODO - Call Ping method and print the results
+
+    my_server_ip = "3.16.215.15"
+    my_rsa_key_file = "C:\\Users\\ALEX\\.ssh\\id_rsa"
+    username = "ubuntu"
+
+    my_upgrade_command = 'sudo apt update && sudo apt upgrade -y'
+    my_server = Server(my_server_ip, my_rsa_key_file, username, my_upgrade_command)
+
+    print('Pinging server %s...' % my_server_ip)
+    ping_result = my_server.ping()
+    print(ping_result)
+    if ping_result == 0:
+        print("Pinging IP [%s] successful." % my_server_ip)
     else:
-        print("Server with ip [%s] is down." % ec2_ip_address)
+        print("Pinging IP [%s] Failed." % my_server_ip)
+
+    print("\nUpdating server using ssh client from paramiko...")
+    ssh_result = my_server.upgrade()
+    print(''.join(ssh_result))
+
+    print('Done.')
